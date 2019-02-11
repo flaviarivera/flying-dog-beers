b3d8ed1b76e85bedeccd7c8d45aaca50f2b1d96b
Flavia Rivera <47529674+flaviarivera@users.noreply.github.com>
2019-02-11 10:55:32 -0500

    changed last beer, colors, change name

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Blue Moon']
 
 bitterness = go.Bar(
     x=beers,
-    y=[35, 60, 85, 85],
+    y=[35, 60, 85, 60],
     name='IBU',
-    marker={'color':'darkblue'}
+    marker={'color':'red'}
 )
 alcohol = go.Bar(
     x=beers,
-    y=[5.4, 7.1, 9.2, 11.5],
+    y=[5.4, 7.1, 9.2, 5.3],
     name='ABV',
-    marker={'color':'blue'}
+    marker={'color':'green'}
 )
 
 beer_data = [bitterness, alcohol]
@@ -33,7 +33,7 @@ app = dash.Dash()
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1('Flying Dog Brewery'),
+    html.H1('Five O'clock'),
     dcc.Graph(
         id='flyingdog',
         figure=beer_fig
